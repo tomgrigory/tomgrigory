6c730a97ac2fda523224cbbe8f9edef38a9b9d53
Tom Grigory <grigorytomgeorge@gmail.com>
2022-02-24 16:44:26 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,9 +34,9 @@ export default function Home() {
         <main className="main-content">
           <p className='h1'>
             hola i'm <a href='#' className='spanh1'>tom grigory</a>. 
-            <br/> <br/> i’m 14 and i’m a <a href='#' className='spanh1'>hacker</a> with super cool <a href='#' className='spanh1'>design skills</a>(atleast my mom thinks so). 
-            <br/> <br/> currently, i’m working on <a href='https://twitter.com/0xohana' target="_blank" className='spanh1'>ohana</a>, a super cool design-first nft marketplace for solana while also building a lil community of devs and designers in web3 at <a href='https://twitter.com/tomgrigory/status/1485269843473362946' target="_blank" className='spanh1'>lil-DAO</a>. 
-            <br/> <br/> you’ll find me attending <a href='https://www.google.com/search?gs_ssp=eJzj4tTP1TewsEgxTzNgtFI1qDBOMjAzSjZPTTa1NExNSUmyMqiwSLE0TUlKTkoxTTI0NUlK8xIqSMzJyczOz09JzFUoTs7Iz88BAMN_Fdo&q=pallikoodam+school&oq=pallik&aqs=chrome.2.69i57j35i39j46i39i175i199j46i512j0i512j46i20i263i512j46i512j0i512l2j46i175i199i512.50033j0j4&sourceid=chrome&ie=UTF-8' target="_blank" className='spanh1'>boring zoom classes</a> by day and <a href='#' className='spanh1'>collecting jpgs + copy pasting code</a> by night. 
+            <br/> <br/> i’m 14 and i’m a <a href='#' className='spanh1'>hacker</a> with super cool <a href='#' className='spanh1'>design skills</a>(atleast my mom thinks so) riding on web3.. 
+            <br/> <br/> currently, i’m working on buidling <a href='https://twitter.com/0xohana' target="_blank" className='spanh1'>ohana</a>, <a href='https://gullak.party' target="_blank" className='spanh1'>gullak and a bunch of other stuff while also training to be a master glasseater🦀.</a>. 
+            <br/> <br/> you’ll find me attending <a href='https://www.google.com/search?gs_ssp=eJzj4tTP1TewsEgxTzNgtFI1qDBOMjAzSjZPTTa1NExNSUmyMqiwSLE0TUlKTkoxTTI0NUlK8xIqSMzJyczOz09JzFUoTs7Iz88BAMN_Fdo&q=pallikoodam+school&oq=pallik&aqs=chrome.2.69i57j35i39j46i39i175i199j46i512j0i512j46i20i263i512j46i512j0i512l2j46i175i199i512.50033j0j4&sourceid=chrome&ie=UTF-8' target="_blank" className='spanh1'>boring classes</a> by day and <a href='#' className='spanh1'>copy pasting code + collecting jpgs</a> by night. 
             <br/> <br/> hmu on <a href='#'className='spanh1'>discord</a> (tomgrigory#4520) or <a href='https://twitter.com/tomgrigory' className='spanh1' target="_blank">twitter</a>, i'm always down for a <a href='#' className='spanh1'>fun call</a>
           </p>
           
